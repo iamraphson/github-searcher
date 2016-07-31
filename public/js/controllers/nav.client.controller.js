bac36284bfb2eb15a2785c1864f29041f0ec9b21
Connor Leech <connorleech@gmail.com>
2016-07-31 11:19:28 -0700

    remove logs

--- public/js/controllers/nav.client.controller.js
+++ public/js/controllers/nav.client.controller.js
@@ -4,12 +4,6 @@
 app.controller('NavbarCtrl', function($scope, $auth, $rootScope){
     if($auth.isAuthenticated()){
         $rootScope.currentUser = $auth.getPayload();
-<<<<<<< HEAD
-        console.log($rootScope.currentUser)
-        console.log($rootScope)
-=======
-        console.log($rootScope.currentUser);
->>>>>>> d16fa6e98f06717d09965818c626395501dbf28a
     }
     $scope.isAuthenticated = function(){
         return $auth.isAuthenticated();
