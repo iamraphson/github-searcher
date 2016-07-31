8549fe1008c2e3e02ae9718ae1ca74010193462f
Ayeni Olusegun <nsegun5@gmail.com>
2016-07-31 16:56:27 +0100

    fix(github-searcher): Fix display bugs

--- public/js/controllers/home.client.controller.js
+++ public/js/controllers/home.client.controller.js
@@ -5,17 +5,23 @@ app.controller('HomeController', function($scope, $auth, spinnerService, gitHubS
     $scope.result = false;
     $scope.gitSearchResponse = []; //Declare an empty array
     $scope.pageno = 1; // initialize page no to 1
-    $scope.total_count = 1000;
+    $scope.total_count = 0;
     $scope.itemsPerPage = 10; //this could be a dynamic value from a drop down
     $scope.search = function(){
+        $scope.result = false;
         $scope.getResultsPage($scope.pageno);
     };
     $scope.getResultsPage = function(pageno) {
         spinnerService.show('html5spinner');
         gitHubService.searchRepo($scope.repo, $scope.itemsPerPage, pageno)
             .then(function(response){
-                $scope.gitSearchResponse = response.data.repos.items;
-                console.log($scope.gitSearchResponse);
+                $scope.gitSearchResponse = response.data.repos;
+                if($scope.gitSearchResponse.items.length > 0)
+                    $scope.total_count = $scope.gitSearchResponse.total_count;
+
+                if($scope.gitSearchResponse.total_count > 1000)
+                    $scope.total_count = 1000;
+
             })
             .catch(function(response){
                 toastr.error(response.data.message, response.status);
