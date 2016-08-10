e1ba3313e6eecbeb4d72c64d9cc3c2e597148c15
Connor Leech <connorleech@gmail.com>
2016-08-09 20:07:53 -0700

    add fallback for development database

--- config/secrets.js
+++ config/secrets.js
@@ -5,5 +5,5 @@ module.exports = {
     TOKEN_SECRET : process.env.TOKEN_SECRET,
     GITHUB_SECRET: process.env.GITHUB_SECRET,
     CACHE_TIMEOUT: (60 * 1440),//86400000
-    db: process.env.MONGO_URL,
+    db: process.env.MONGO_URL || 'mongodb://localhost/github-searcher',
 };
