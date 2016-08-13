1804de5e2567add4a43a8bf3b237404e2688c106
Ayeni Olusegun <nsegun5@gmail.com>
2016-08-13 20:13:26 +0100

    mongodb port

--- config/secrets.js
+++ config/secrets.js
@@ -5,5 +5,5 @@ module.exports = {
     TOKEN_SECRET : process.env.TOKEN_SECRET,
     GITHUB_SECRET: process.env.GITHUB_SECRET,
     CACHE_TIMEOUT: (60 * 1440),//86400000
-    db: process.env.MONGO_URL || 'mongodb://localhost/github-searcher',
+    db: process.env.MONGO_URL || 'mongodb://localhost:27017/github-searcher',
 };
